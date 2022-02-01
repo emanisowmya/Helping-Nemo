3a7250e5431f1110579e7243ea9be7828c63f5d9
AMITKESARI2000 <kesari.amit33@gmail.com>
2022-02-01 13:03:40 +0530

    Menu add story-survival

--- GameD/Assets/Scripts/startnemo.cs
+++ GameD/Assets/Scripts/startnemo.cs
@@ -1,38 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartNemo : MonoBehaviour
 {
-  // Start is called before the first frame update
-  private int speed = 2;
-  private float start;
-  private float end;
-  void Start()
-  {
-    Vector2 pos = transform.position;
-    start = pos.x;
-    end = pos.y;
-  }
+    // Start is called before the first frame update
+    private int speed = 2;
+    private float start;
+    private float end;
+    void Start()
+    {
+        Vector2 pos = transform.position;
+        start = pos.x;
+        end = pos.y;
+    }
 
-  // Update is called once per frame
-  void Update()
-  {
-    Vector2 pos = transform.position;
-    if (pos.x < start / 2)
+    // Update is called once per frame
+    void Update()
     {
-      pos.x += speed * Time.deltaTime;
-      transform.position = pos;
-    }
-    else if (pos.y > 0)
-    {
+        Vector2 pos = transform.position;
+        if (pos.x < start / 2)
+        {
+            pos.x += speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else if (pos.y > 0)
+        {
 
-      pos.y -= speed * Time.deltaTime;
-      transform.position = pos;
+            pos.y -= speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
-    else
-    {
-      // SceneManager.LoadScene("flappyBird");
-    }
-  }
 }
