7bcab2edf4675deab42b9542cdef9a8117e52b69
AMITKESARI2000 <kesari.amit33@gmail.com>
2022-02-02 16:56:43 +0530

    menu functions finalised

--- GameD/Assets/Scripts/TransMenu.cs
+++ GameD/Assets/Scripts/TransMenu.cs
@@ -6,25 +6,43 @@ public class TransMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Button storyModeButton;
+    [SerializeField] private Button survivalModeButton;
     void Start()
     {
-        Button btn = storyModeButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        //load story mode when clicked
+        Button btnStory = storyModeButton.GetComponent<Button>();
+        btnStory.onClick.AddListener(StoryModeOnClick);
+
+        //load survival mode when clicked
+        Button btnSurvival = survivalModeButton.GetComponent<Button>();
+        btnSurvival.onClick.AddListener(SurvivalModeOnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // for story mode we can click button or press 1 or press Enter
         if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Return))
         {
-            TaskOnClick();
+            StoryModeOnClick();
+        }
+        // for survival mode we can click button or press 2
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SurvivalModeOnClick();
         }
     }
 
-    void TaskOnClick()
+    void StoryModeOnClick()
     {
         SceneManager.LoadScene("Nemo Intro");
     }
+
+    void SurvivalModeOnClick()
+    {
+        SceneManager.LoadScene("SurvivalLevel");
+    }
+
 
     public void QuitGame()
     {
